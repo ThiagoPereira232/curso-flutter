b00df77c7eae2b0d65e08421a8a589361bb09d40
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-04-02 13:00:55 -0300

    alinhamentos

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -4,27 +4,17 @@ void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
     home: Container(
-      //color: Colors.white,
       decoration: BoxDecoration(
         border: Border.all(width: 3, color: Colors.red)
       ),
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-      // margin: EdgeInsets.only(left: 50, top: 50),
-      margin: EdgeInsets.all(0),
-      child: Column(
+      margin: EdgeInsets.only(top:40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("t1"),
-          
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: Text("t2"),
-          ),
-
+          Text("t2"),
           Text("t3"),
-          /*Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus, lectus vel congue viverra",
-            textAlign: TextAlign.justify,
-          )*/
         ],
       ),
     ),
