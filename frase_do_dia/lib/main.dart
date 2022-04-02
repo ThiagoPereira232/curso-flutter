e140d9ca3022bfc2eb83ea7abd83c0165b0868fc
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-04-02 12:52:57 -0300

    espaçamentos

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -4,23 +4,29 @@ void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
     home: Container(
-      color: Colors.white,
+      //color: Colors.white,
+      decoration: BoxDecoration(
+        border: Border.all(width: 3, color: Colors.red)
+      ),
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      // margin: EdgeInsets.only(left: 50, top: 50),
+      margin: EdgeInsets.all(0),
       child: Column(
         children: [
-          TextButton(
-            onPressed: (){
-              print("Botão pressionado!");
-            }, 
-            child: Text(
-              "Clica aqui",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                decoration: TextDecoration.none
-              )
-              )
-            )
-        ])
-      ,),
+          Text("t1"),
+          
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Text("t2"),
+          ),
+
+          Text("t3"),
+          /*Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus, lectus vel congue viverra",
+            textAlign: TextAlign.justify,
+          )*/
+        ],
+      ),
+    ),
   ));
 }
