f30430d646fa86fcca35c10a8d0d5c38a49c08bd
Thiago Pereira <pereira.tp.thiago@gmail.com>
2022-04-02 13:41:58 -0300

    imagens

--- frase_do_dia/lib/main.dart
+++ frase_do_dia/lib/main.dart
@@ -4,19 +4,14 @@ void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
     home: Container(
+      margin: EdgeInsets.only(top:40),
       decoration: BoxDecoration(
         border: Border.all(width: 3, color: Colors.red)
       ),
-      margin: EdgeInsets.only(top:40),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("t1"),
-          Text("t2"),
-          Text("t3"),
-        ],
-      ),
+      child: Image.asset(
+        "images/parque.jpg",
+        fit: BoxFit.scaleDown,
+      )
     ),
   ));
 }
