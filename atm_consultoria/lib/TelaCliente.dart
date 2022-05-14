5f2f0be91e84cb51945a10021e0b4bb31c03a0b5
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-14 15:33:54 -0300

    Update TelaCliente.dart

--- atm_consultoria/lib/TelaCliente.dart
+++ atm_consultoria/lib/TelaCliente.dart
@@ -10,6 +10,50 @@ class TelaCliente extends StatefulWidget {
 class _TelaClienteState extends State<TelaCliente> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Cliente"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset("images/detalhe_cliente.png"),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Clientes",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    )
+                  )
+                ]
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Image.asset("images/cliente1.png"),
+              ),
+              const Text(
+                "Empresa de software"
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Image.asset("images/cliente2.png"),
+              ),
+              const Text(
+                "Empresa de auditoria"
+              )
+            ],
+          )
+        ),
+      )
+    );
   }
 }
