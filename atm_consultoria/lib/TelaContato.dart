403118748ad056cc50e5a6ee6ff81ba599497ebb
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-05-14 15:39:15 -0300

    Update TelaContato.dart

--- atm_consultoria/lib/TelaContato.dart
+++ atm_consultoria/lib/TelaContato.dart
@@ -10,6 +10,48 @@ class TelaContato extends StatefulWidget {
 class _TelaContatoState extends State<TelaContato> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Contato"),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset("images/detalhe_contato.png"),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Contato",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    )
+                  )
+                ]
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text("atendimento@atmconsultoria.com.br")
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text("Telefone: (11) 3525-8596")
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text("Telefone: (11) 99586-5236")
+              )
+            ],
+          )
+        ),
+      )
+    );
   }
 }
